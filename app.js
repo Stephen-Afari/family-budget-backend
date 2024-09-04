2373b1fc5c3047e1ad78fa2aec50732c88888d2f
Stephen Afari <demorganafari@yahoo.com>
2024-09-04 04:53:02 +0000

    update

--- app.js
+++ app.js
@@ -2,14 +2,37 @@ const express = require('express');
 const app = express();
 const path = require("path");
 const morgan = require('morgan'); //morgan is a middleware function that logs HTTP requests and responses in a Node.js application. It provides a simple way to track the incoming requests and the status of the outgoing responses, which can be very useful for debugging and monitoring the application's behavior.
-
-
+const rateLimit = require('express-rate-limit'); //prevent same ip from making too many requests
+const helmet = require('helmet'); //security best practices
+const mongoSanitize = require('express-mongo-sanitize'); //clean data from malicious code.
+const xss = require('xss-clean');
+const hpp = require('hpp');
+const cors = require('cors');
+const globalErrorHandler = require('./controllers/errorController');
+const AppError = require('./utils/appError');
 
 const actualIncRouter = require('./routes/actualIncomeRoutes');
 const actualTrxnRouter = require('./routes/actualTransactionRoutes');
 const budgetIncRouter = require('./routes/budgetIncomeRoutes');
 const budgetTrxnRouter = require('./routes/budgetTransactionRoutes');
 const userRouter = require('./routes/userRoutes');
+
+//1) GLOBAL MIDDLEWARES
+// Implement CORS
+app.use(cors());
+// Access-Control-Allow-Origin *
+// api.natours.com, front-end natours.com
+// app.use(cors({
+//   origin: 'https://www.natours.com'
+// }))
+//The line app.options('*', cors()); is used in a Node.js/Express application to handle preflight requests for Cross-Origin Resource Sharing (CORS).
+//Preflight requests are a type of request sent by the browser when a web application running in one origin (like https://example.com) requests resources from a different origin (like https://api.example.com).
+//The app.options('*', cors()); line is configuring your Express application to respond to all OPTIONS requests with the appropriate CORS headers, regardless of the path. This is particularly useful for handling CORS preflight requests.
+app.options('*', cors());
+
+// Set security HTTP headers
+//Using Helmet is a quick and easy way to improve the security of your Express application. It helps protect your app from some common security vulnerabilities without requiring you to manually configure each individual header. It's especially useful for preventing XSS, clickjacking, and other injection attacks.
+app.use(helmet());
 
 // 1) MIDDLEWARES
 //use morgan for logging
@@ -18,10 +41,42 @@ if(process.env.NODE_ENV='development'){
   app.use(morgan('dev'))
 }
 
+// Limit requests from same API
+//prevent same ip from making too many requests
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!'
+});
+app.use('/api', limiter);
+
 //express.json(): This is a built-in middleware function in Express that parses incoming requests with JSON payloads and is based on body-parser. It extracts the JSON data from the request body and makes it available under req.body.
 //When a request is sent to the server, if the request’s Content-Type is application/json, express.json() middleware will automatically parse the JSON-formatted request body.
 //The parsed JSON object is then assigned to req.body so that it can be accessed in your route handlers.
-app.use(express.json());
+//app.use(express.json());
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' })); //limit the message body to 10kb
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+//app.use(cookieParser());
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS--cross site scripting attacks
+app.use(xss());
+
+// Prevent parameter pollution-- to clean the query string
+//whitelist to allow duplicates in the query string
+app.use(
+  hpp({
+    whitelist: [
+      'parent',
+      'subGroup',
+      'description'
+      
+    ]
+  })
+);
 
 //custom middleware in an Express.js application (ie. app.use())
 //Middleware functions are functions that have access to the request object (req), the response object (res), and the next function in the application's request-response cycle.
@@ -44,11 +99,11 @@ app.use('/api/v1/budginc',budgetIncRouter);
 app.use('/api/v1/budgtrxn',budgetTrxnRouter);
 app.use('/api/v1/users',userRouter);
 
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
 
-
-
-
-
+app.use(globalErrorHandler);
 
 
 
